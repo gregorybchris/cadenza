69a5c695c738403481b3c9f12089a72fbbf45766
Chris Gregory <gregorybchris@gmail.com>
2025-01-12 23:09:32 -0800

    Clean up audio synth

--- src/cadenza/audio.py
+++ src/cadenza/audio.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 from pathlib import Path
 
 import sounddevice
@@ -6,54 +7,70 @@ import torchaudio
 from torch import Tensor
 
 
+@dataclass(kw_only=True)
+class Saver:
+    sample_rate: int
+
+    def save(self, audio: Tensor, filepath: Path) -> None:
+        audio_tensor = audio.unsqueeze(0)  # Convert to 2D tensor (1 channel)
+        torchaudio.save(str(filepath), audio_tensor, self.sample_rate)
+
+
+@dataclass(kw_only=True)
+class Player:
+    sample_rate: int
+
+    def play(self, audio: Tensor) -> None:
+        sounddevice.play(audio.numpy(), samplerate=self.sample_rate)
+        sounddevice.wait()
+
+
+@dataclass(kw_only=True)
+class SynthArgs:
+    sample_rate: int
+
+
+@dataclass(kw_only=True)
+class Synth:
+    args: SynthArgs
+
+    def generate(self, frequencies: list[float], duration: float) -> Tensor:
+        # Time vector
+        t = torch.linspace(0, duration, int(self.args.sample_rate * duration), dtype=torch.float32)
+
+        # Generate sine waves
+        amplitude = 0.5
+        tones = torch.stack([amplitude * torch.sin(2 * torch.pi * freq * t) for freq in frequencies])
+
+        # Merge tones by summing them
+        audio = tones.sum(dim=0)
+
+        # Add overtones with exponential decay
+        for freq in frequencies:
+            for i in range(2, 10):
+                overtone = freq * i
+                decay = 1 / i**2
+                overtone_amplitude = decay * amplitude
+                overtone_waveform = overtone_amplitude * torch.sin(2 * torch.pi * overtone * t)
+                audio += overtone_waveform
+
+        # Normalize to prevent clipping
+        audio /= len(frequencies)
+        return audio
+
+
 def get_frequency(reference_frequency: float, distance: int) -> float:
     return reference_frequency * 2 ** (distance / 12)
 
 
-def save_audio(audio: Tensor, filepath: Path, sample_rate: int) -> None:
-    audio_tensor = audio.unsqueeze(0)  # Convert to 2D tensor (1 channel)
-    torchaudio.save(str(filepath), audio_tensor, sample_rate)
-
-
-def play_audio(audio: Tensor, sample_rate: int) -> None:
-    sounddevice.play(audio.numpy(), samplerate=sample_rate)
-    sounddevice.wait()
-
-
-def generate_audio(frequencies: list[float], duration: float, sample_rate: int) -> Tensor:
-    # Time vector
-    t = torch.linspace(0, duration, int(sample_rate * duration), dtype=torch.float32)
-
-    # Generate sine waves
-    amplitude = 0.5
-    tones = torch.stack([amplitude * torch.sin(2 * torch.pi * freq * t) for freq in frequencies])
-
-    # Merge tones by summing them
-    combined_waveform = tones.sum(dim=0)
-
-    # Add overtones with exponential decay
-    for freq in frequencies:
-        for i in range(2, 10):
-            overtone = freq * i
-            decay = 1 / i**2
-            overtone_amplitude = decay * amplitude
-            overtone_waveform = overtone_amplitude * torch.sin(2 * torch.pi * overtone * t)
-            combined_waveform += overtone_waveform
-
-    # Normalize to prevent clipping
-    combined_waveform /= len(frequencies)
-
-    return combined_waveform
-
-
 if __name__ == "__main__":
-    sample_rate = 44100
+    sample_rate = 44_100
 
-    duration = 2.0
-
-    reference_frequency = 440.0  # A4
+    synth_args = SynthArgs(sample_rate=sample_rate)
+    synth = Synth(args=synth_args)
 
     # Am chord
+    reference_frequency = 440.0  # A4
     frequencies = [
         reference_frequency,  # A4
         get_frequency(reference_frequency, 3),  # C4
@@ -61,7 +78,11 @@ if __name__ == "__main__":
         get_frequency(reference_frequency, 12),  # A5
     ]
 
-    audio = generate_audio(frequencies, duration, sample_rate)
-    play_audio(audio, sample_rate)
+    duration = 2.0
+    audio = synth.generate(frequencies, duration)
+    player = Player(sample_rate=sample_rate)
+    player.play(audio)
+
+    saver = Saver(sample_rate=sample_rate)
     audio_filepath = Path("data/output.wav")
-    save_audio(audio, audio_filepath, sample_rate)
+    saver.save(audio, audio_filepath)
