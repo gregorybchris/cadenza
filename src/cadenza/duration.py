12b45698ea02db30e867ed00f75eda40d2aeb57e
Chris Gregory <gregorybchris@gmail.com>
2025-01-12 23:18:16 -0800

    Add note durations

---
+++ src/cadenza/duration.py
@@ -0,0 +1,10 @@
+from enum import StrEnum, auto
+
+
+class Duration(StrEnum):
+    Whole = auto()
+    Half = auto()
+    Quarter = auto()
+    Eighth = auto()
+    Sixteenth = auto()
+    ThirtySecond = auto()
