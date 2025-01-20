b226a7fcf305a65142e8f9d836f7ddbbc495d85a
Chris Gregory <chris@hume.ai>
2025-01-20 03:20:11 -0500

    Implement crossfading when concatenating segments

--- src/cadenza/synth.py
+++ src/cadenza/synth.py
@@ -47,7 +47,29 @@ class Synth:
         return audio
 
     def concat(self, segments: list[Tensor]) -> Tensor:
-        return torch.cat(segments)
+        fade_duration_s = 0.05
+        sample_rate = self.args.sample_rate
+        max_fade_samples = int(fade_duration_s * sample_rate)
+
+        segments = [seg for seg in segments if len(seg) > 0]
+
+        new_segments = [segments[0]]
+        for i in range(1, len(segments)):
+            seg_a = segments[i - 1]
+            seg_b = segments[i]
+
+            # Handle case where segments are shorter than fade duration
+            fade_samples = min(max_fade_samples, len(seg_a), len(seg_b))
+
+            fade_out = torch.linspace(1, 0, fade_samples, dtype=torch.float32)
+            seg_a[-fade_samples:] *= fade_out
+
+            fade_in = torch.linspace(0, 1, fade_samples, dtype=torch.float32)
+            seg_b[:fade_samples] *= fade_in
+
+            new_segments.append(seg_b[fade_samples:])
+
+        return torch.cat(new_segments)
 
     def generate_pitch_audio(
         self,
