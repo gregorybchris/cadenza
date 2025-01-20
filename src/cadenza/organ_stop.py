830083006eee35cf4328418583c7efd0d61de49e
Chris Gregory <chris@hume.ai>
2025-01-20 02:42:41 -0500

    Fix organ stop multipliers

--- src/cadenza/organ_stop.py
+++ src/cadenza/organ_stop.py
@@ -5,19 +5,17 @@ logger = logging.getLogger(__name__)
 
 
 class OrganStop(StrEnum):
-    OneFoot = auto()
     TwoFoot = auto()
     FourFoot = auto()
     EightFoot = auto()
     SixteenFoot = auto()
     ThirtyTwoFoot = auto()
+    SixtyFourFoot = auto()
 
     def get_multiplier(self) -> float:
         match self:
-            case OrganStop.OneFoot:
-                return 1 / 8
             case OrganStop.TwoFoot:
-                return 1 / 4
+                return 1 / 3
             case OrganStop.FourFoot:
                 return 1 / 2
             case OrganStop.EightFoot:
@@ -25,12 +23,12 @@ class OrganStop(StrEnum):
             case OrganStop.SixteenFoot:
                 return 2
             case OrganStop.ThirtyTwoFoot:
+                return 3
+            case OrganStop.SixtyFourFoot:
                 return 4
 
     def get_decay(self) -> float:
         match self:
-            case OrganStop.OneFoot:
-                return 1 / 16
             case OrganStop.TwoFoot:
                 return 1 / 9
             case OrganStop.FourFoot:
@@ -41,3 +39,5 @@ class OrganStop(StrEnum):
                 return 1 / 4
             case OrganStop.ThirtyTwoFoot:
                 return 1 / 9
+            case OrganStop.SixtyFourFoot:
+                return 1 / 16
